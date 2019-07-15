b94c62ddac4f499bd8bbf5e871411c67e024c863
atomispectre <52656718+atomispectre@users.noreply.github.com>
2019-07-15 15:50:57 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require('header.php'); ?>
+
+    <main role="main" class="container margin">
+
+      <div class="starter-template">
+        <h1>Atomi Spectre</h1>
+        <p class="lead">Great people build better stuff.</p>
+      </div>
+
+    </main>
+
+<?php require('footer.php');?>
---
+++ sandbox.php
@@ -0,0 +1,13 @@
+<?php require('header.php');?>
+
+<main class="container margin">
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">Atomi Spectre</h1>
+            <p class="lead">Great people build better stuff.</p>
+        </div>
+    </div>
+</main>
+
+<?php require('footer.php');?>
+
